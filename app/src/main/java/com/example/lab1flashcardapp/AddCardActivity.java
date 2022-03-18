ca236805fc61608d1ce9d8fc1948e795af35cc82
Njeriiii <joymwenjwa@gmail.com>
2022-03-19 07:51:57 +0900

    Lab2!
    Lab2!

---
+++ app/src/main/java/com/example/lab1flashcardapp/AddCardActivity.java
@@ -0,0 +1,46 @@
+package com.example.lab1flashcardapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class AddCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_card);
+
+        findViewById(R.id.image_remove_button).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                    finish();
+            }
+        });
+
+        ImageView saveQuestionImageView = findViewById(R.id.image_save_button);
+        saveQuestionImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent data = new Intent();
+                String inputQuestion = ((EditText) findViewById(R.id.setQuestion)).getText().toString();
+                String inputAnswer = ((EditText) findViewById(R.id.inputAnswer)).getText().toString();
+                Log.d("AddCardActivity", "hi" + inputQuestion + ", " + inputAnswer );
+                data.putExtra("QuestionKey",inputQuestion);
+                data.putExtra("AnswerKey",inputAnswer);
+                String str = data.getExtras().getString("QuestionKey");
+                Log.d("AddCardActivity", "hi" + str );
+                setResult(RESULT_OK, data);
+                finish();
+
+            }
+        });
+
+
+    }
+}
--- app/src/main/java/com/example/lab1flashcardapp/MainActivity.java
+++ app/src/main/java/com/example/lab1flashcardapp/MainActivity.java
@@ -1,20 +1,30 @@
 package com.example.lab1flashcardapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView flashcardQuestion;
+    TextView flashcardAnswer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView flashcardQuestion = findViewById(R.id.flashcard_question);
-        TextView flashcardAnswer = findViewById(R.id.flashcard_answer);
+        flashcardQuestion = findViewById(R.id.flashcard_question);
+        flashcardAnswer = findViewById(R.id.flashcard_answer);
         flashcardQuestion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -22,6 +32,33 @@ public class MainActivity extends AppCompatActivity {
                 flashcardAnswer.setVisibility(View.VISIBLE);
             }
         });
+
+        ImageView addQuestionImageView = findViewById(R.id.image_add_button);
+        addQuestionImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+                startActivityForResult(intent, 100);
+            }
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d("onActivityResult", "Success" + data.getExtras().getString("QuestionKey"));
+        if (requestCode == 100) {
+            //get data
+            Log.d("onActivityResult", "requestCode == 100");
+            if (data != null) {//check if there's an intent
+
+                String QuestionString = data.getExtras().getString("QuestionKey");
+                String AnswerString = data.getExtras().getString("AnswerKey");
+                flashcardQuestion.setText(QuestionString);
+                flashcardAnswer.setText(AnswerString);
+                Log.d("onActivityResult", QuestionString + "; " + AnswerString);
+            }
+        }
     }
 }
-
